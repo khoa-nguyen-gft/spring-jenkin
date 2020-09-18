6d58deeaa24a9324378b3e78b2a322e36974a9c1
khoa <npdangkhoa@gmail.com>
2020-09-18 14:40:32 +0700

    init

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    public static Logger logger = LoggerFactory.getLogger(DemoApplicationTests.class);
+    
+	@Test
+	void contextLoads() {
+        logger.info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>test case executing...");
+        assertThat(true);
+
+	}
+
+}
